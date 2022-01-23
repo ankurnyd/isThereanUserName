eb72ae20c0b1bf6b651c03521281e0ff2da24bdf
Ankur Sharma <38402246+ankurnyd@users.noreply.github.com>
2022-01-24 04:34:38 +0530

    Done

---
+++ main.js
@@ -0,0 +1,8 @@
+let username = '';
+
+username="ankur0123"
+
+// Use short circuit evaluation to assign defaultUsername variable below:
+let defaultUsername = username || 'defaultUser';
+
+console.log(`The ${defaultUsername} is the username.`);
